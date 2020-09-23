5bf45a70a610c72899f3f21635505780362bd52e
Daniel Lousa <dlousa92@gmail.com>
2020-09-23 19:09:32 -0400

    create global styles and typography

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Nav from "./Nav";
 import Footer from "./Footer";
+import "normalize.css";
+import GlobalStyles from "../styles/GlobalStyles";
+import Typography from "../styles/Typography";
 
 let Layout = (props) => {
   return (
     <div>
+      <GlobalStyles />
+      <Typography />
       <Nav />
       {props.children}
       <Footer />
