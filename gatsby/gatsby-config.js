bf524914a647960b2d985e40b48d5909dba5b2a1
Daniel Lousa <dlousa92@gmail.com>
2020-10-06 14:18:53 -0400

    fix congfig

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,7 +1,5 @@
 import dotenv from "dotenv";
 
-dotenv.config({ path: ".env" });
-
 export default {
   siteMetadata: {
     title: `Slicks Slices`,
@@ -9,11 +7,11 @@ export default {
     description: "The best pizza place in town",
   },
   plugins: [
-    // plug ins can be a string or passed in as an object if you would like to add additional settins.
+    // plugins can be a string or passed in as an object if you would like to add additional settins.
     "gatsby-plugin-styled-components",
     {
       // this is the name of the plugin you are adding
-      resole: "gatsby-source-sanity",
+      resolve: "gatsby-source-sanity",
       options: {
         projectId: "ztbc4p4x",
         dataset: "production",
